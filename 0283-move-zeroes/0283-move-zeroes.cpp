58277de77e20d210d997b05a698010f4956e6786
Santosh Ray <iamsantoshray27@gmail.com>
2026-08-03 01:30:26 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int fast = 0;
+        int slow = 0;
+
+        while (fast < n) {
+            if (nums[fast] != 0) {
+                swap(nums[slow], nums[fast]);
+                slow++;
+            }
+            fast++;
+        }
+    }
+};
