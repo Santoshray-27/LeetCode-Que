1323701a2675a4d9d208a9086a464cb52b47417e
Santosh Ray <iamsantoshray27@gmail.com>
2026-08-09 23:26:35 +0530

    Time: 0 ms (100%), Space: 31.4 MB (47.08%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int st = 0;
-        int end = n - 1;
-        int mid;
-        while (st <= end) {
-            mid = st + (end - st) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-                ;
+        int left = 0; // initialize left pointer to 0
+        int right = nums.size() - 1; // initialize right pointer to the last index of the array
+        
+        while (left <= right) { // continue the loop till left pointer is less than or equal to right pointer
+            int mid = left + (right - left) / 2; // calculate the middle index of the array
+            
+            if (nums[mid] == target) { // check if the middle element is equal to target
+                return mid; // return the middle index
+            } else if (nums[mid] < target) { // check if the middle element is less than target
+                left = mid + 1; // move the left pointer to the right of middle element
+            } else { // if the middle element is greater than target
+                right = mid - 1; // move the right pointer to the left of middle element
             }
         }
-        return -1;
+        
+        return -1; // target not found in the array
     }
 };
